f9f9e6ae4e6de4c673e63aa42a6ebb806f51089d
Roman <romochkaarefev@list.ru>
2025-02-06 18:17:10 +0300

    version 0.1

--- mian.py
+++ mian.py
@@ -3,5 +3,15 @@ import random
 
 a = random.randint(1, 100)
 
+while True:
+    print(a)
+    num = int(input("Chose number:"))
+    if a == num:
+        print("You win!!!")
+        break
+    elif a < num:
+        print("Много")
+    elif a > num:
+        print("Мало")
 
-print(a)
+print("Exit")
